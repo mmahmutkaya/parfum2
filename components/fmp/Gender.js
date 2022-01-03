f8b8847fe2522f277cfa4fdf981ac525de6d1b74
Mahmut Kaya <mmahmutkaya@gmail.com>
2022-01-03 22:37:43 +0300

    next image public ok

--- components/fmp/Gender.js
+++ components/fmp/Gender.js
@@ -1,11 +1,8 @@
 import Image from "next/image";
 
-// import woman from "../../public/images/woman2.png"
+const woman = "/images/woman2.png"
 // import man from "../../public/images/dog1.png";
 
-const myLoader = ({ src, width, quality }) => {
-  return `https://res.cloudinary.com/dal2s2do5/image/upload/v1641233498/samples/parfume2/0/woman2_qv4ttr.png`;
-};
 
 const Gender = () => {
   const handleClick = ({ gender }) => {
@@ -22,18 +19,18 @@ const Gender = () => {
           className="transition ease-in-out hover:scale-110 hover:border-red-700 duration-1000"
           width={300}
           height={500}
-          src={'/images/dog1.png'}
+          src={'/images/man2.png'}
         />
       </div>
 
-      {/* <div onClick={() => handleClick({ gender: "man" })}>
+      <div onClick={() => handleClick({ gender: "man" })}>
         <Image
           className="transition ease-in-out hover:scale-110 hover:border-red-700 duration-1000"
           width={300}
           height={500}
-          src={man}
+          src={woman}
         />
-      </div> */}
+      </div>
 
 
     </div>
